e80af1efeef2c0cd1b2afb8556464a17bd26ffb7
mac.komarov <mac.komarov@ea022c65-b648-0410-8974-0d324702c27d>
2011-03-09 11:41:18 +0000

    Tagged 0.7.17

---
+++ scalarizr/tags/0.7.17/src/scalarizr/handlers/ec2/lifecycle.py
@@ -0,0 +1,114 @@
+'''
+Created on Mar 2, 2010
+
+@author: marat
+'''
+from scalarizr.bus import bus
+from scalarizr.handlers import Handler
+from scalarizr.util import system2, filetool, disttool
+import logging
+import os, re
+import ConfigParser
+from scalarizr.util.fstool import Mtab, Fstab, mount
+
+
+def get_handlers ():
+	return [Ec2LifeCycleHandler()]
+
+class Ec2LifeCycleHandler(Handler):
+	_logger = None
+	_platform = None
+	"""
+	@ivar scalarizr.platform.ec2.Ec2Platform:
+	"""
+	
+	def __init__(self):
+		self._logger = logging.getLogger(__name__)
+		self._platform = bus.platform
+		bus.on("init", self.on_init)		
+	
+	def on_init(self, *args, **kwargs):
+		bus.on("before_hello", self.on_before_hello)		
+		bus.on("before_host_init", self.on_before_host_init)
+		bus.on("before_restart", self.on_before_restart)
+
+		msg_service = bus.messaging_service
+		producer = msg_service.get_producer()
+		producer.on("before_send", self.on_before_message_send)
+		
+		# Set the hostname to this instance's public hostname
+		cnf = bus.cnf
+		try:
+			hostname_as_pubdns = int(cnf.rawini.get('ec2', 'hostname_as_pubdns'))
+		except ConfigParser.Error:
+			hostname_as_pubdns = True
+		if hostname_as_pubdns:
+			system2("hostname " + self._platform.get_public_hostname(), shell=True)		
+		
+		if disttool.is_ubuntu():
+			# Ubuntu cloud-init scripts may disable root ssh login
+			for path in ('/etc/ec2-init/ec2-config.cfg', '/etc/cloud/cloud.cfg'):
+				if os.path.exists(path):
+					c = filetool.read_file(path)
+					c = re.sub(re.compile(r'^disable_root[^:=]*([:=]).*', re.M), r'disable_root\1 0', c)
+					filetool.write_file(path, c)
+			
+		# Add server ssh public key to authorized_keys
+		authorized_keys_path = "/root/.ssh/authorized_keys"
+		if os.path.exists(authorized_keys_path):
+			c = filetool.read_file(authorized_keys_path)
+			ssh_key = self._platform.get_ssh_pub_key()
+			if c.find(ssh_key) == -1:
+				c += ssh_key + "\n"
+				self._logger.debug("Add server ssh public key to authorized_keys")
+				filetool.write_file(authorized_keys_path, c)
+				
+		# Mount ephemeral devices
+		# Seen on eucalyptus: 
+		# 	- fstab contains invalid fstype and `mount -a` fails  
+		mtab = Mtab()
+		fstab = Fstab()
+		for device in ('/dev/sda2', '/dev/sdb'):
+			if os.path.exists(device) and fstab.contains(device) and not mtab.contains(device):
+				entry = fstab.find(device)[0]
+				mount(device, entry.mpoint, ('-o', entry.options))
+		
+
+	
+	def on_before_hello(self, message):
+		"""
+		@param message: Hello message
+		"""
+		
+		message.aws_instance_id = self._platform.get_instance_id()
+		message.aws_instance_type = self._platform.get_instance_type()		
+		message.aws_ami_id = self._platform.get_ami_id()
+		message.aws_avail_zone = self._platform.get_avail_zone()
+
+
+	def on_before_host_init(self, message):
+		"""
+		@param message: HostInit message
+		"""
+
+		message.ssh_pub_key = self._platform.get_ssh_pub_key()
+
+	def on_before_restart(self, message):
+		"""
+		@param message: Restart message
+		@type message: scalarizr.messaging.Message 
+		"""
+		
+		"""
+		@todo Update ips, reset platform meta-data
+ 		@see http://docs.amazonwebservices.com/AWSEC2/latest/DeveloperGuide/index.html?Concepts_BootFromEBS.html#Stop_Start
+		"""
+		pass
+
+	def on_before_message_send(self, queue, message):
+		"""
+		@todo: add aws specific here
+		"""
+		
+		pass
+	
---
+++ scalarizr/tags/0.7.17/src/scalarizr/platform/ec2/__init__.py
@@ -0,0 +1,102 @@
+
+from scalarizr.bus import bus
+from scalarizr.platform import Ec2LikePlatform, PlatformError
+from scalarizr.storage.transfer import Transfer
+from .storage import S3TransferProvider
+
+from boto import connect_ec2, connect_s3
+from boto.ec2.regioninfo import RegionInfo
+import urllib2, re, os
+
+
+Transfer.explore_provider(S3TransferProvider)
+
+
+"""
+Platform configuration options
+"""
+OPT_ACCOUNT_ID = "account_id"
+OPT_KEY = "key"
+OPT_KEY_ID = "key_id"
+OPT_EC2_CERT_PATH = "ec2_cert_path"
+OPT_CERT_PATH = "cert_path"
+OPT_PK_PATH = "pk_path"
+
+
+"""
+User data options 
+"""
+UD_OPT_S3_BUCKET_NAME = "s3bucket"
+
+
+def get_platform():
+	return Ec2Platform()
+
+class Ec2Platform(Ec2LikePlatform):
+	name = "ec2"
+
+	_userdata_key = "latest/user-data"
+
+	ec2_endpoints = {
+		"us-east-1" 		: "ec2.amazonaws.com",
+		"us-west-1" 		: "ec2.us-west-1.amazonaws.com",
+		"eu-west-1" 		: "ec2.eu-west-1.amazonaws.com",
+		"ap-southeast-1" 	: "ec2.ap-southeast-1.amazonaws.com",
+		"ap-northeast-1" 	: "ec2.ap-northeast-1.amazonaws.com"
+	}
+	s3_endpoints = {
+		'us-east-1' 		: 's3.amazonaws.com',
+		'us-west-1' 		: 's3-us-west-1.amazonaws.com',
+		'eu-west-1' 		: 's3.amazonaws.com',
+		'ap-southeast-1' 	: 's3-ap-southeast-1.amazonaws.com',
+		'ap-northeast-1' 	: 's3-ap-northeast-1.amazonaws.com'
+	}	
+
+	_logger = None	
+	_ec2_cert = None
+	_cnf = None
+	
+	def get_account_id(self):
+		return self.get_access_data("account_id").encode("ascii")
+			
+	def get_access_keys(self):
+		# Keys must be in ASCII because hmac functions doesn't works with unicode		
+		return (self.get_access_data("key_id").encode("ascii"), self.get_access_data("key").encode("ascii"))
+			
+	def get_cert_pk(self):
+		return (self.get_access_data("cert").encode("ascii"), self.get_access_data("pk").encode("ascii"))
+	
+	def get_ec2_cert(self):
+		if not self._ec2_cert:
+			# XXX: not ok
+			self._ec2_cert = self._cnf.read_key(os.path.join(bus.etc_path, self._cnf.rawini.get(self.name, OPT_EC2_CERT_PATH)), title="EC2 certificate")
+		return self._ec2_cert
+	
+	def new_ec2_conn(self):
+		""" @rtype: boto.ec2.connection.EC2Connection """
+		region = self.get_region()
+		self._logger.debug("Return ec2 connection (endpoint: %s)", self.ec2_endpoints[region])
+		return connect_ec2(region=RegionInfo(name=region, endpoint=self.ec2_endpoints[region]))
+
+	def new_s3_conn(self):
+		self._logger.debug("Return s3 connection (endpoint: %s)", self.s3_endpoints[self.get_region()])
+		return connect_s3(host=self.s3_endpoints[self.get_region()])
+	
+	def set_access_data(self, access_data):
+		Ec2LikePlatform.set_access_data(self, access_data)
+		key_id, key = self.get_access_keys()
+		os.environ['AWS_ACCESS_KEY_ID'] = key_id
+		os.environ['AWS_SECRET_ACCESS_KEY'] = key
+
+	def clear_access_data(self):
+		Ec2LikePlatform.clear_access_data(self)
+		try:
+			del os.environ['AWS_ACCESS_KEY_ID']
+			del os.environ['AWS_SECRET_ACCESS_KEY']
+		except KeyError:
+			pass
+		
+	@property
+	def cloud_storage_path(self):
+		return self.get_user_data('cloud_storage_path') or 's3://' + self.get_user_data(UD_OPT_S3_BUCKET_NAME)
+
